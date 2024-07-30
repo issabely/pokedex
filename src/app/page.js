e65de3bcd34c8db5d4a6a2c1272e55040a8106f4
issabely <gisabely093@gmail.com>
2024-07-30 00:03:05 -0300

    mostrando cartas para usuarios

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,81 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from '@/app/page.module.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={styles.mainContainer}>
+      <div className={styles.carouselContainer}>
+        <div className={styles.cardContainer}>
+          <div>pokemon</div>
+          <div className={styles.imageContainer}>
+            <img
+              src="https://img.pokemondb.net/sprites/black-white/anim/normal/pikachu.gif"
+              alt="pokemon gif"
+              height={100}
+              width={100} />
+          </div>
+          <div className={styles.descriptionContainer}>
+            <div className={styles.attributesContainer}>
+              <div>tipo</div>
+              <div>eletrico</div>
+            </div>
+            <div className={styles.attributesContainer}>
+              <div>atq</div>
+              <div>10</div>
+            </div>
+            <div className={styles.attributesContainer}>
+              <div>def</div>
+              <div>10</div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className={styles.cardContainer}>
+          <div>pokemon</div>
+          <div className={styles.imageContainer}>
+            <img
+              src="https://img.pokemondb.net/sprites/black-white/anim/normal/pikachu.gif"
+              alt="pokemon gif"
+              height={100}
+              width={100} />
+          </div>
+          <div className={styles.descriptionContainer}>
+            <div className={styles.attributesContainer}>
+              <div>tipo</div>
+              <div>eletrico</div>
+            </div>
+            <div className={styles.attributesContainer}>
+              <div>atq</div>
+              <div>10</div>
+            </div>
+            <div className={styles.attributesContainer}>
+              <div>def</div>
+              <div>10</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.cardContainer}>
+          <div>pokemon</div>
+          <div className={styles.imageContainer}>
+            <img
+              src="https://img.pokemondb.net/sprites/black-white/anim/normal/pikachu.gif"
+              alt="pokemon gif"
+              height={100}
+              width={100} />
+          </div>
+          <div className={styles.descriptionContainer}>
+            <div className={styles.attributesContainer}>
+              <div>tipo</div>
+              <div>eletrico</div>
+            </div>
+            <div className={styles.attributesContainer}>
+              <div>atq</div>
+              <div>10</div>
+            </div>
+            <div className={styles.attributesContainer}>
+              <div>def</div>
+              <div>10</div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
